6359a9e9782ac10e0df61b626ad03ed1d831b5a4
dahiya301 <sachinbeasian@gmail.com>
2026-06-27 14:40:54 +0530

    Refactor scroll event listener and initialize Swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,37 @@
-window.addEventListener('scroll',()=>{
+const heroSwiper = new Swiper(".heroSwiper", {
 
-const nav=document.querySelector('.navbar');
+loop:true,
+
+speed:1200,
+
+effect:"fade",
+
+autoplay:{
+delay:5000,
+disableOnInteraction:false
+},
+
+pagination:{
+el:".swiper-pagination",
+clickable:true
+},
+
+navigation:{
+nextEl:".swiper-button-next",
+prevEl:".swiper-button-prev"
+}
+
+});
+
+window.addEventListener("scroll",()=>{
+
+const nav=document.querySelector(".navbar");
 
 if(window.scrollY>50){
-nav.style.background='#111';
-}else{
-nav.style.background='transparent';
+nav.style.background="#111";
+}
+else{
+nav.style.background="transparent";
 }
 
 });
